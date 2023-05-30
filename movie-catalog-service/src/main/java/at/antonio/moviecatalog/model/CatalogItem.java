a7c738637396b222eedae530eb65976ab41c5da6
Antonio BITOUNIS <antonio.bitounis@rbinternational.com>
2023-05-30 13:02:18 +0200

    API setup

---
+++ movie-catalog-service/src/main/java/at/antonio/moviecatalog/model/CatalogItem.java
@@ -0,0 +1,16 @@
+package at.antonio.moviecatalog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CatalogItem {
+    private String name;
+    private String desc;
+    private int rating;
+}
---
+++ movie-catalog-service/src/test/java/at/antonio/moviecatalog/MovieCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package at.antonio.moviecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
